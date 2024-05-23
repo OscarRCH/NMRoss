b2abc6607752cca27ff44120756969928f82c58c
CedricRossboth <cedric.rossboth@epfl.ch>
2024-05-23 13:00:31 +0200

    Following set up commit

---
+++ src/nmross/__init__.py
@@ -0,0 +1,5 @@
+"""Gives an approximate 1H NMR when given a smiles or a IUPAC name of a molecule with maximum one aromatic ring and no double bonds.."""
+
+from __future__ import annotations
+
+__version__ = "0.0.1"
